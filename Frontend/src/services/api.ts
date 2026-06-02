b77f0c102967df926a09cdd3ced9743dd2722e5d
suji-vtrios <suji.sugathan@vtrios.com>
2026-06-02 22:09:31 +0530

    Debug API URL

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
-const api = axios.create({
+console.log(
+  'VITE_API_URL =',
+  import.meta.env.VITE_API_URL
+)
 
-  baseURL:
-    import.meta.env
-      .VITE_API_URL
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export default api
