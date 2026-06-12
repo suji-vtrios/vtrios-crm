451a51fe37497ee3ddbc74686445a5db3b7d9d40
suji-vtrios <suji.sugathan@vtrios.com>
2026-06-12 12:52:32 +0530

    wati prompt fix

--- Backend/app/services/knowledge_loader.py
+++ Backend/app/services/knowledge_loader.py
@@ -5,18 +5,28 @@ def load_knowledge():
 
     content = ""
 
-    knowledge_path = Path(
-        "knowledge"
+    knowledge_path = (
+        Path(__file__)
+        .parent.parent
+        / "knowledge"
+    )
+
+    print(
+        "KNOWLEDGE PATH =",
+        knowledge_path
     )
 
     for file in knowledge_path.rglob(
         "*.md"
     ):
 
-        content += (
-            file.read_text(
-                encoding="utf-8"
-            )
+        print(
+            "LOADING =",
+            file
+        )
+
+        content += file.read_text(
+            encoding="utf-8"
         )
 
         content += "\n\n"
